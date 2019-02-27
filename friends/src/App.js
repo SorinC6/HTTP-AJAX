8cff94f68e32ffbe27be68e9150e8a3efb6106ea
Sorin Chis <kish.sorin@yahoo.com>
2019-02-27 14:07:57 +0200

    using axios to get data from the server

---
+++ friends/src/App.js
@@ -0,0 +1,47 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+
+import './App.css';
+
+class App extends Component {
+	state = {
+		friends: [],
+		error: null,
+		loading: false
+	};
+
+	componentDidMount() {
+      this.startSpinner();
+		axios
+			.get('http://localhost:5000/friends')
+			.then((res) => this.setFriends(res.data))
+			.catch((err) => this.setError(err));
+	}
+
+	setFriends = (friend) => {
+      this.stopSpinner();
+		this.setState({ friends: friend });
+	};
+
+	setError = (err) => {
+		this.setState({ error: err });
+	};
+
+	startSpinner = () => {
+		this.setState({ loading: true });
+	};
+
+	stopSpinner = () => {
+		this.setState({ loading: false });
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<h1>Friends App Lambda HTTP-AJAX</h1>
+			</div>
+		);
+	}
+}
+
+export default App;
